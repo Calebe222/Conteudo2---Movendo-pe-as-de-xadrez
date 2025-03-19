83eed249820cfa70cd543f2020ea7fe372702d0a
Calebe222 <136852396+Calebe222@users.noreply.github.com>
2025-03-19 14:50:25 +0000

    exercicíos loops

---
+++ loop aninhados/exer3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main () {
+    for (int i = 1; i <= 10; i++) { // loop externo
+        for (int j = 1; j <= 10; i++) {
+            printf("%d \n", i * j);
+        }
+
+        
+    }
+}
