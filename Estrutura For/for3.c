a539f18e483f7900cd90ffcdf45b7fd87ba4aae0
Calebe222 <136852396+Calebe222@users.noreply.github.com>
2025-03-19 04:31:55 +0000

    adição de novos exercícios

---
+++ Estrutura For/for3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main () {
+
+    // Loop que começa em 0 e vai até 100, incrementando de 10 em 10
+    for (int i = 0; i <= 100; i += 10) {
+        // Imprime o valor atual de i
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
